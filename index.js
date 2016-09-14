583ebcec9985baf2e02843f5029ea5df3431e886
Douglas Linder <linderd@iinet.net.au>
2016-09-14 19:55:27 +0800

    errors in clojure files no longer break gulp

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
 'use strict'
 
-const gutil = require('gulp-util')
-const through = require('through2')
-const objectAssign = require('object-assign')
-const cljs = require('clojurescript')
-const GulpClojureError = require('./GulpClojureError')
-const replaceExtension = require('./lib/replaceExtension')
-
-
+const gutil = require('gulp-util');
+const through = require('through2');
+const objectAssign = require('object-assign');
+const cljs = require('clojurescript');
+const GulpClojureError = require('./GulpClojureError');
+const replaceExtension = require('./lib/replaceExtension');
 
 function transform (file, enc, callback) {
 	if (file.isNull()) { return callback(null, file) }
 
 	if (file.isStream()) {
 		let err = new GulpClojureError('Streaming not supported')
-
 		this.emit('error', err)
 		return callback()
 	}
 
 	try {
-		let compiledJS = cljs.compile(file.contents.toString('utf-8'))
-
-		file.contents = new Buffer(compiledJS)
+		let compiledJS = cljs.compile(file.contents.toString('utf-8'));
+		file.contents = new Buffer(compiledJS);
 	} catch (err) {
 		let msg = Array.isArray(err) ? err.join('\n') : err
-		let errorInfo = {
-			fileName: file.path
-		, showStack: false
-		}
-
-		let err = new GulpClojureError(msg, errorInfo)
-		this.emit('error', err)
+		let errorInfo = { fileName: file.path, showStack: false }
+		let error = new GulpClojureError(msg, errorInfo)
+		this.emit('error', error)
 	}
 
-
 	file.path = replaceExtension(file.path)
-
 	this.push(file)
 	callback()
 }
--- test/index.js
+++ test/index.js
@@ -1,32 +1,33 @@
 'use strict'
 
-const clj = require('../')
-const fs = require('fs')
-const should = require('chai').should()
-const gutil = require('gulp-util')
-const createFile = require('../lib/createFile')
+const clj = require('../');
+const fs = require('fs');
+const should = require('chai').should();
+const gutil = require('gulp-util');
+const createFile = require('../lib/createFile');
 
-const base = './test/fixtures'
-const testFilePath = `${base}/test.txt`
-const clojureFilePath = `${base}/app.clj`
-const compiledJsFixturePath =  `${base}/app.js`
+const base = './test/fixtures';
+const testFilePath = `${base}/test.txt`;
+const clojureFilePath = `${base}/app.clj`;
+const compiledJsFixturePath =  `${base}/app.js`;
+const invalidFilePath =  `${base}/invalid.clj`;
 
 
 describe('gulp-clj', () => {
 
     it('should emit an error when passed a stream', function(done) {
-      let filepath = './test/src/app.clj'
-      let stream = fs.ReadStream(filepath)
+      let filepath = './test/src/app.clj';
+      let stream = fs.ReadStream(filepath);
 
       clj()
         .on('error', err => {
-          err.message.should.equal('Streaming not supported')
-          done()
+          err.message.should.equal('Streaming not supported');
+          done();
         })
         .on('data', newFile => {
-					throw new Error('No file should have been emitted!')
+					throw new Error('No file should have been emitted!');
 				})
-        .write(createFile(filepath, stream))
+        .write(createFile(filepath, stream));
     })
 
 		it('should replace the .clj extension with .js',  done => {
@@ -37,8 +38,19 @@ describe('gulp-clj', () => {
 				.on('error', err => { throw err })
 				.on('data', newFile => {
 					newFile.extname.should.equal('.js')
-					done()
+					done();
 				})
-				.write(file)
+				.write(file);
 		})
+
+    it('should error on invalid file content',  done => {
+      let contents = fs.readFileSync(invalidFilePath)
+      let file = createFile(invalidFilePath, contents)
+
+      clj()
+        .on('error', err => {
+          done();
+        })
+        .write(file);
+    })
 })
